ea9b76a036a4de63876ae5e1a062542f5fc13f86
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-28 15:48:06 -0600

    segundo ejercicio con for

--- Actividad 14 Maldonado Andy/Program.cs
+++ Actividad 14 Maldonado Andy/Program.cs
@@ -1,10 +1,10 @@
-﻿// cuadrado de 5x5 con números 1.
+﻿// rectángulo de 4 filas y 6 columnas con asteriscos.
 
-for(int fila =0; fila < 5; fila++)
+for (int fila =0; fila < 4; fila++)
 {
-    for(int col = 0; col < 5; col++)
+    for(int col = 0; col < 6; col++)
     {
-        Console.Write("1 ");
+        Console.Write("* ");
     }
     Console.WriteLine();
 }
