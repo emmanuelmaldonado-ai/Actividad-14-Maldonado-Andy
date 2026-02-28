93a24ab4c3b260f65929c6001b1cccf0fbfe8129
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-28 15:45:17 -0600

    primer ejercicio con for

--- Actividad 14 Maldonado Andy/Program.cs
+++ Actividad 14 Maldonado Andy/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// cuadrado de 5x5 con números 1.
+
+for(int fila =0; fila < 5; fila++)
+{
+    for(int col = 0; col < 5; col++)
+    {
+        Console.Write("1 ");
+    }
+    Console.WriteLine();
+}
