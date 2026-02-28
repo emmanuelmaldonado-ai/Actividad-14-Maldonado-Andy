a667448a87be224fbe9f39a8be1f9a8cc72f1db0
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-28 17:50:55 -0600

    decimo ejercicio usando for anidado

--- Actividad 14 Maldonado Andy/Program.cs
+++ Actividad 14 Maldonado Andy/Program.cs
@@ -1,10 +1,14 @@
-﻿// patrón donde cada fila tenga el número de la fila repetido.
+﻿// Mostrar un patrón numérico como:
+//1
+//22
+//333
+//4444
 
-for (int i =1; i <=4; i++)
+for (int i =1; i <=8; i++)
 {
-    for(int j =1; j <= 4; j++)
+    for(int j =1; j <= i; j++)
     {
-        Console.Write(i);
+        Console.Write($"{i}");
     }
     Console.WriteLine();
 }
