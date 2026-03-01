65e346a689e1ee73c101edd3a7434bda8d8c5523
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-28 19:08:01 -0600

    ejercicio 14 usando while

--- Actividad 14 Maldonado Andy/Program.cs
+++ Actividad 14 Maldonado Andy/Program.cs
@@ -1,6 +1,7 @@
-﻿// Mostrar los números pares del 1 al 20.
-int i =2;
-while(i <= 20)
+﻿// Mostrar los números impares del 1 al 20.
+
+int i =1;
+while (i <= 20)
 {
     Console.WriteLine(i);
     i += 2;
