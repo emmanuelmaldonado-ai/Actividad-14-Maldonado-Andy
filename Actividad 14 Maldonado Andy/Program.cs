ab4b3d4ba2d3033ab533072b2885342a0d80454a
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-03-01 00:24:43 -0600

    ejercicio 19 usando while

--- Actividad 14 Maldonado Andy/Program.cs
+++ Actividad 14 Maldonado Andy/Program.cs
@@ -1,13 +1,11 @@
-﻿// Calcular el factorial de un número ingresado.
+﻿// Mostrar la tabla de multiplicar de un número ingresado.
 
 Console.WriteLine("Ingrese un número");
 int num = int.Parse(Console.ReadLine());
-int fact = 1;
 int i = 1;
 
-while(i <= num)
+while(i <= 10)
 {
-    fact *= i;
+Console.WriteLine($"{num}x{i} = {num * i}");
     i++;
 }
-Console.WriteLine($"El factorial de {num} es {fact}");
